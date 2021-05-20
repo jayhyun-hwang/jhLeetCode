c63ad2b1f91f5fa9640890f0bc1bd05b722730aa
jayhyun-hwang <67496978+jayhyun-hwang@users.noreply.github.com>
2021-05-20 12:58:35 +0900

    Update 228.summary-ranges.go

--- 228.summary-ranges.go
+++ 228.summary-ranges.go
@@ -19,29 +19,30 @@ func summaryRanges(nums []int) []string {
 	}
 	return obj.res
 }
+
 func (obj *NumsObj) recRange(nums []int, i int) {
 	if len(nums)-1 == i {
-		if obj.intArr[0] >= obj.intArr[1] {
-			obj.res = append(obj.res, strconv.Itoa(obj.intArr[0]))
-		} else {
-			obj.res = append(obj.res, strconv.Itoa(obj.intArr[0])+"->"+strconv.Itoa(obj.intArr[1]))
-		}
+		obj.insertStr()
 		return
 	}
 	if nums[i]+1 != nums[i+1] {
-		var strEle string
-		if obj.intArr[0] >= obj.intArr[1] {
-			strEle = strconv.Itoa(obj.intArr[0])
-		} else {
-			strEle = strconv.Itoa(obj.intArr[0]) + "->" + strconv.Itoa(obj.intArr[1])
-		}
-		obj.res = append(obj.res, strEle)
+		obj.insertStr()
 		obj.intArr[0] = nums[i+1]
 	} else {
 		obj.intArr[1] = nums[i+1]
 	}
 	i++
 	obj.recRange(nums, i)
+}
+
+func (obj *NumsObj) insertStr() {
+	var strEle string
+    	if obj.intArr[0] >= obj.intArr[1] {
+    	    strEle = strconv.Itoa(obj.intArr[0])
+    	} else {
+	        strEle = strconv.Itoa(obj.intArr[0]) + "->" + strconv.Itoa(obj.intArr[1])
+    	}
+    	obj.res = append(obj.res, strEle)
 }
 
 // @lc code=end
