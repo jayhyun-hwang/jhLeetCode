6ffbf8f9e904393e5c002b9ec0b6bab83632e3d5
jayhyun-hwang <67496978+jayhyun-hwang@users.noreply.github.com>
2021-05-24 00:54:16 +0900

    no message

--- 389.find-the-difference.go
+++ 389.find-the-difference.go
@@ -6,7 +6,16 @@
 
 // @lc code=start
 func findTheDifference(s string, t string) byte {
-    
+	if len(s) == 0 {
+		return t[0]
+	}
+	for i := 0; i < len(s); i++ {
+		if s[i] != t[i] {
+			return t[i]
+		}
+	}
+	return t[len(s)]
 }
+
 // @lc code=end
 
