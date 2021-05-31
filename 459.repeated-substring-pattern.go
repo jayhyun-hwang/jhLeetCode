648cfe5b7a568a0bdaba75d0085aac519779ddfd
jayhyun-hwang <67496978+jayhyun-hwang@users.noreply.github.com>
2021-05-31 20:58:30 +0900

    459

--- 459.repeated-substring-pattern.go
+++ 459.repeated-substring-pattern.go
@@ -5,19 +5,29 @@
  */
 
 // @lc code=start
-
 type Obj struct {
 	strLen int
 }
 
 func repeatedSubstringPattern(s string) bool {
 	obj := new(Obj)
-	obj.str
+	obj.strLen = 1
 	return obj.recurFunc(s)
 }
 func (obj *Obj) recurFunc(s string) bool {
-	if len(s) 
-	
+	if len(s)%obj.strLen != 0 {
+		obj.strLen++
+		return obj.recurFunc(s)
+	}
+	if obj.strLen >= len(s)/2+1 {
+		return false
+	}
+	for i := 1; obj.strLen*i < len(s); i++ {
+		if s[:obj.strLen] != s[obj.strLen*i:obj.strLen*(i+1)] {
+			obj.strLen++
+			return obj.recurFunc(s)
+		}
+	}
 	return true
 }
 
--- testCode/main_test.go
+++ testCode/main_test.go
@@ -18,7 +18,8 @@ func TestMain(t *testing.T) {
 	str := "abcabcabcd"
 	sStr := strings.Split(str, str[:1])
 	fmt.Println(sStr)
-	_ = repeatedSubstringPattern("anananb")
+	b := repeatedSubstringPattern("ababab")
+	_ = b
 	//fmt.Println(summaryRanges([]int{-1})) //print testFunc return value
 }
 
@@ -36,12 +37,14 @@ func (obj *Obj) recurFunc(s string) bool {
 		obj.strLen++
 		return obj.recurFunc(s)
 	}
-	if obj.strLen == len(s)/2+1 {
+	if obj.strLen >= len(s)/2+1 {
 		return false
 	}
-	for i := 1; obj.strLen*i > len(s); i++ {
-
+	for i := 1; obj.strLen*i < len(s); i++ {
+		if s[:obj.strLen] != s[obj.strLen*i:obj.strLen*(i+1)] {
+			obj.strLen++
+			return obj.recurFunc(s)
+		}
 	}
-
 	return true
 }
