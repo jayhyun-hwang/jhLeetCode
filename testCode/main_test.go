e3eb021f338362446eefe70703d48f581b865331
jayhyun-hwang <67496978+jayhyun-hwang@users.noreply.github.com>
2021-06-13 23:45:24 +0900

    quick sort

--- testCode/main_test.go
+++ testCode/main_test.go
@@ -19,6 +19,29 @@ func TestMain(t *testing.T) {
 
 }
 
+func quickSort(arr []int, left int, right int) {
+	if left < right {
+		pivot := partition(arr, left, right)
+
+		quickSort(arr, left, pivot-1)
+		quickSort(arr, pivot+1, right)
+	}
+}
+func partition(arr []int, left int, right int) int {
+	pivot := arr[right]
+	i := left - 1
+
+	for j := left; j <= right-1; j++ {
+		if arr[j] <= pivot {
+			i++
+			//swap
+			arr[i], arr[j] = arr[j], arr[i]
+		}
+	}
+	arr[i+1], arr[right] = arr[right], arr[i+1]
+	return i + 1
+}
+
 // tf := "test param" //test function here
 // tf := "ghdfasdf" //test function here
 // bArr := []byte(tf)
