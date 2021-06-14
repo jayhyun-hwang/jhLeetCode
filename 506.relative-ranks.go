7f66482c755530189010629919d70e27f047fc0a
jayhyun-hwang <67496978+jayhyun-hwang@users.noreply.github.com>
2021-06-14 21:42:46 +0900

    506

--- 506.relative-ranks.go
+++ 506.relative-ranks.go
@@ -6,8 +6,32 @@
 
 // @lc code=start
 func findRelativeRanks(score []int) []string {
-    
-	
+	mp := make(map[int]string)
+	res := make([]string, 0)
+	scoreCopy := make([]int, len(score))
+	copy(scoreCopy, score)
+	sort.Slice(scoreCopy, func(i, j int) bool {
+		return scoreCopy[i] > scoreCopy[j]
+	})
+	for idx, val := range scoreCopy {
+		var ele string
+		switch idx {
+		case 0:
+			ele = "Gold Medal"
+		case 1:
+			ele = "Silver Medal"
+		case 2:
+			ele = "Bronze Medal"
+		default:
+			ele = strconv.Itoa(idx + 1)
+		}
+		mp[val] = ele
+	}
+	for _, val := range score {
+		res = append(res, mp[val])
+	}
+	return res
 }
+
 // @lc code=end
 
--- testCode/main_test.go
+++ testCode/main_test.go
@@ -2,87 +2,53 @@ package main_test
 
 import (
 	"fmt"
-	"log"
-	"net/http"
-	_ "net/http/pprof"
+	"sort"
+	"strconv"
 	"testing"
 )
 
-type TreeNode struct {
-	Val   int
-	Left  *TreeNode
-	Right *TreeNode
+func findRelativeRanks(score []int) []string {
+	mp := make(map[int]string)
+	res := make([]string, len(score))
+	scoreCopy := make([]int, len(score))
+	copy(scoreCopy, score)
+	sort.Slice(scoreCopy, func(i, j int) bool {
+		return scoreCopy[i] > scoreCopy[j]
+	})
+	for idx, val := range scoreCopy {
+		var ele string
+		switch idx {
+		case 0:
+			ele = "Gold Medal"
+		case 1:
+			ele = "Silver Medal"
+		case 2:
+			ele = "Bronze Medal"
+		default:
+			ele = strconv.Itoa(idx + 1)
+		}
+		mp[val] = ele
+	}
+	for _, val := range score {
+		res = append(res, mp[val])
+	}
+	return res
 }
 
-func findMode(root *TreeNode) []int {
-	return nil
-}
-
-type WorkResult struct {
-	UserID  int64
-	Wt1Time int32
-	Payment int64
+func findRelativeRanks22(score []int) []string {
+	fmt.Printf("score: %v\n", score)
+	sort.Slice(score, func(i, j int) bool {
+		return score[i] < score[j]
+	})
+	fmt.Printf("score: %v\n", score)
+	return []string{"finish", "ed"}
 }
 
 //main
 func TestMain(t *testing.T) {
-	param1 := "a"
-	param2 := "c"
-	wWorkResults := []WorkResult{}
-	for i := 0; i < 100; i++ {
-		w := new(WorkResult)
-		w.UserID = int64(i % 10)
-		w.Wt1Time = int32(i)
-		w.Payment = int64(i * 1000)
-		wWorkResults = append(wWorkResults, *w)
-	}
-	wWorkResults2 := make([]WorkResult, 100)
-	var totalWorkTime int32
-	var totalPayment int64
-	totalWorkTime = 0
-	totalPayment = 0
-	if param1 != "" {
-		//이름으로 검색할 경우, 여러 id의 사용자가 조회될 수 있다. 이럴 때는 total 수치를 넘겨주지 않는다.
-		if param2 == "b" {
-			//검색된 id의 개수를 체크하기위한 map
-			checkUserCountMp := make(map[int64]bool)
-			isOneUser := true
-			for idx, wWorkResult := range wWorkResults {
-				tnaResult := wWorkResult
-				if isOneUser {
-					totalWorkTime += wWorkResult.Wt1Time
-					totalPayment += wWorkResult.Payment
-					if !checkUserCountMp[wWorkResult.UserID] {
-						checkUserCountMp[wWorkResult.UserID] = true
-						if len(checkUserCountMp) > 1 {
-							isOneUser = false
-							totalWorkTime = 0
-							totalPayment = 0
-						}
-					}
-				}
-				wWorkResults2[idx] = tnaResult
-			}
-			fmt.Println("1 finished")
-		} else {
-			for idx, wWorkResult := range wWorkResults {
-				tnaResult := wWorkResult
-				totalWorkTime += wWorkResult.Wt1Time
-				totalPayment += wWorkResult.Payment
-				wWorkResults2[idx] = tnaResult
-			}
-			fmt.Println("2 finished")
-		}
-	} else {
-		//total 연산없이 진행
-		for idx, wWorkResult := range wWorkResults {
-			tnaResult := wWorkResult
-			wWorkResults2[idx] = tnaResult
-		}
-		fmt.Println("3 finished")
-	}
-
-	log.Println(http.ListenAndServe("localhost:6060", nil))
+	intArr := []int{1, 11, 4, 32, 5, 6, 53, 86}
+	res := findRelativeRanks(intArr)
+	fmt.Printf("res: %v\n", res)
 }
 
 // tf := "test param" //test function here
