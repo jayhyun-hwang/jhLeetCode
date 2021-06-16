c688126414ae542b60b7c8858f775831e2f248de
jayhyun-hwang <wogus0501@gmail.com>
2021-06-16 18:00:44 +0900

    auto commit

--- testCode/main_test.go
+++ testCode/main_test.go
@@ -10,23 +10,24 @@ import (
 func findRelativeRanks(score []int) []string {
 	//map for matching scores and ranks
 	mp := make(map[int]string)
-	res := make([]string, len(score))
+	res := make([]string, 0)
 	scoreCopy := make([]int, len(score))
 	copy(scoreCopy, score)
 	sort.Slice(scoreCopy, func(i, j int) bool {
-		return scoreCopy[i] > scoreCopy[j]
+		return scoreCopy[i] < scoreCopy[j]
 	})
+	lenCopy := len(scoreCopy)
 	for idx, val := range scoreCopy {
 		var ele string
 		switch idx {
-		case 0:
+		case lenCopy - 1:
 			ele = "Gold Medal"
-		case 1:
+		case lenCopy - 2:
 			ele = "Silver Medal"
-		case 2:
+		case lenCopy - 3:
 			ele = "Bronze Medal"
 		default:
-			ele = strconv.Itoa(idx + 1)
+			ele = strconv.Itoa(lenCopy - idx)
 		}
 		mp[val] = ele
 	}
@@ -36,10 +37,39 @@ func findRelativeRanks(score []int) []string {
 	return res
 }
 
+// func findRelativeRanks(score []int) []string {
+// 	//map for matching scores and ranks
+// 	mp := make(map[int]string)
+// 	res := make([]string, 0)
+// 	scoreCopy := make([]int, len(score))
+// 	copy(scoreCopy, score)
+// 	sort.Slice(scoreCopy, func(i, j int) bool {
+// 		return scoreCopy[i] > scoreCopy[j]
+// 	})
+// 	for idx, val := range scoreCopy {
+// 		var ele string
+// 		switch idx {
+// 		case 0:
+// 			ele = "Gold Medal"
+// 		case 1:
+// 			ele = "Silver Medal"
+// 		case 2:
+// 			ele = "Bronze Medal"
+// 		default:
+// 			ele = strconv.Itoa(idx + 1)
+// 		}
+// 		mp[val] = ele
+// 	}
+// 	for _, val := range score {
+// 		res = append(res, mp[val])
+// 	}
+// 	return res
+// }
+
 func findRelativeRanks22(score []int) []string {
 	fmt.Printf("score: %v\n", score)
 	sort.Slice(score, func(i, j int) bool {
-		return score[i] < score[j]
+		return score[i] > score[j]
 	})
 	fmt.Printf("score: %v\n", score)
 	return []string{"finish", "ed"}
@@ -47,8 +77,10 @@ func findRelativeRanks22(score []int) []string {
 
 //main
 func TestMain(t *testing.T) {
-	intArr := []int{1, 11, 4, 32, 5, 6, 53, 86}
-	res := findRelativeRanks(intArr)
+	intArr := []int{1, 11, 4, 32, 1, 5, 6, 53, 11, 86}
+	var res []string
+	//res = findRelativeRanks22(intArr)
+	res = findRelativeRanks(intArr)
 	fmt.Printf("res: %v\n", res)
 }
 
