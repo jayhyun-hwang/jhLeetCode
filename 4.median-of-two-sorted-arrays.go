6bf204fd65d7dcb8536a56d617f2b5bbe6079864
jayhyun-hwang <wogus0501@gmail.com>
2021-06-23 18:01:53 +0900

    auto commit

--- 4.median-of-two-sorted-arrays.go
+++ 4.median-of-two-sorted-arrays.go
@@ -6,7 +6,20 @@
 
 // @lc code=start
 func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
-    
+	res := 0.0
+	nums1 = append(nums1, nums2...)
+	sort.Slice(nums1, func(i, j int) bool {
+		return nums1[i] < nums1[j]
+	})
+	nlen := len(nums1)
+	median := int(nlen / 2)
+	if nlen%2 == 1 {
+		res = float64(nums1[median])
+	} else {
+		res = float64(nums1[median-1]+nums1[median]) / 2
+	}
+	return res
 }
+
 // @lc code=end
 
--- testCode/main_test.go
+++ testCode/main_test.go
@@ -1,90 +1,58 @@
 package testCode
 
 import (
-	"fmt"
 	"sort"
-	"strconv"
 	"testing"
 )
 
 //main
 func TestMain(t *testing.T) {
-	intArr := []int{1, 11, 4, 32, 1, 5, 6, 53, 11, 86}
-	var res []string
-	//res = findRelativeRanks22(intArr)
-	res = findRelativeRanks(intArr)
-	fmt.Printf("res: %v\n", res)
-
-	res2 := reverseInt(2345)
-	fmt.Printf("res2: %v\n", res2)
+	print(findMedianSortedArrays([]int{1, 1}, []int{1, 2}))
 }
-func findRelativeRanks(score []int) []string {
-	//map for matching scores and ranks
-	mp := make(map[int]string)
-	res := make([]string, 0)
-	scoreCopy := make([]int, len(score))
-	copy(scoreCopy, score)
-	sort.Slice(scoreCopy, func(i, j int) bool {
-		return scoreCopy[i] < scoreCopy[j]
+func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
+	res := 0.0
+	numsSum := append(nums1, nums2...)
+	sort.Slice(numsSum, func(i, j int) bool {
+		return numsSum[i] < numsSum[j]
 	})
-	lenCopy := len(scoreCopy)
-	for idx, val := range scoreCopy {
-		var ele string
-		switch idx {
-		case lenCopy - 1:
-			ele = "Gold Medal"
-		case lenCopy - 2:
-			ele = "Silver Medal"
-		case lenCopy - 3:
-			ele = "Bronze Medal"
-		default:
-			ele = strconv.Itoa(lenCopy - idx)
-		}
-		mp[val] = ele
-	}
-	for _, val := range score {
-		res = append(res, mp[val])
+	nlen := len(numsSum)
+	median := int(nlen / 2)
+	if nlen%2 == 1 {
+		res = float64(numsSum[median])
+	} else {
+		res = float64(numsSum[median-1]+numsSum[median]) / 2
 	}
 	return res
 }
 
-// func findRelativeRanks(score []int) []string {
-// 	//map for matching scores and ranks
-// 	mp := make(map[int]string)
-// 	res := make([]string, 0)
-// 	scoreCopy := make([]int, len(score))
-// 	copy(scoreCopy, score)
-// 	sort.Slice(scoreCopy, func(i, j int) bool {
-// 		return scoreCopy[i] > scoreCopy[j]
-// 	})
-// 	for idx, val := range scoreCopy {
-// 		var ele string
-// 		switch idx {
-// 		case 0:
-// 			ele = "Gold Medal"
-// 		case 1:
-// 			ele = "Silver Medal"
-// 		case 2:
-// 			ele = "Bronze Medal"
-// 		default:
-// 			ele = strconv.Itoa(idx + 1)
+// func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
+// 	mp := make(map[int]bool)
+// 	numsSum := make([]int, 0)
+// 	res := 0.0
+// 	for _, val := range nums1 {
+// 		if !mp[val] {
+// 			numsSum = append(numsSum, val)
+// 			mp[val] = true
 // 		}
-// 		mp[val] = ele
 // 	}
-// 	for _, val := range score {
-// 		res = append(res, mp[val])
+// 	for _, val := range nums2 {
+// 		if !mp[val] {
+// 			numsSum = append(numsSum, val)
+// 			mp[val] = true
+// 		}
+// 	}
+// 	sort.Slice(numsSum, func(i, j int) bool {
+// 		return numsSum[i] < numsSum[j]
+// 	})
+// 	nlen := len(numsSum)
+// 	median := int(nlen / 2)
+// 	if nlen%2 == 1 {
+// 		res = float64(numsSum[median])
+// 	} else {
+// 		res = float64(numsSum[median-1]+numsSum[median]) / 2
 // 	}
 // 	return res
 // }
-
-func findRelativeRanks22(score []int) []string {
-	fmt.Printf("score: %v\n", score)
-	sort.Slice(score, func(i, j int) bool {
-		return score[i] > score[j]
-	})
-	fmt.Printf("score: %v\n", score)
-	return []string{"finish", "ed"}
-}
 
 // tf := "test param" //test function here
 // tf := "ghdfasdf" //test function here
