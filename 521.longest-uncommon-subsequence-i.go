ba7b6e1f7339a39f0262e009145862b1979fc4f8
jayhyun-hwang <67496978+jayhyun-hwang@users.noreply.github.com>
2021-06-28 00:41:03 +0900

    no message

--- 521.longest-uncommon-subsequence-i.go
+++ 521.longest-uncommon-subsequence-i.go
@@ -6,7 +6,14 @@
 
 // @lc code=start
 func findLUSlength(a string, b string) int {
-    
+	if a == b {
+		return -1
+	}
+	if len(a) > len(b) {
+		return len(a)
+	}
+	return len(b)
 }
+
 // @lc code=end
 
--- 530.minimum-absolute-difference-in-bst.go
+++ 530.minimum-absolute-difference-in-bst.go
@@ -13,8 +13,23 @@
  *     Right *TreeNode
  * }
  */
+var Min = 1000000
+
 func getMinimumDifference(root *TreeNode) int {
-    
+	if root.Right != nil {
+		if root.Right.Val-root.Val < Min {
+			Min = root.Right.Val - root.Val
+		}
+		Min = getMinimumDifference(root.Right)
+	}
+	if root.Left != nil {
+		if root.Val-root.Left.Val < Min {
+			Min = root.Val - root.Left.Val
+		}
+		Min = getMinimumDifference(root.Left)
+	}
+	return Min
 }
+
 // @lc code=end
 
