963d49784a9fe095bf1aeeaa18e5ea3d15a6fd27
jayhyun-hwang <wogus0501@gmail.com>
2021-06-28 15:04:43 +0900

    auto commit

--- 530.minimum-absolute-difference-in-bst.go
+++ 530.minimum-absolute-difference-in-bst.go
@@ -16,20 +16,46 @@
 var Min = 1000000
 
 func getMinimumDifference(root *TreeNode) int {
-	if root.Right != nil {
-		if root.Right.Val-root.Val < Min {
-			Min = root.Right.Val - root.Val
+	min := math.MaxInt64
+	preVal := math.MinInt32
+
+	helper := func(n *TreeNode) {}
+	helper = func(n *TreeNode) {
+		if n == nil {
+			return
 		}
-		Min = getMinimumDifference(root.Right)
-	}
-	if root.Left != nil {
-		if root.Val-root.Left.Val < Min {
-			Min = root.Val - root.Left.Val
+
+		helper(n.Left)
+		if preVal != math.MinInt32 {
+			d := n.Val - preVal
+			if d < min {
+				min = d
+			}
 		}
-		Min = getMinimumDifference(root.Left)
+
+		preVal = n.Val
+		helper(n.Right)
 	}
-	return Min
+
+	helper(root)
+	return min
 }
+
+// func getMinimumDifference(root *TreeNode) int {
+// 	if root.Right != nil {
+// 		if root.Right.Val-root.Val < Min {
+// 			Min = root.Right.Val - root.Val
+// 		}
+// 		Min = getMinimumDifference(root.Right)
+// 	}
+// 	if root.Left != nil {
+// 		if root.Val-root.Left.Val < Min {
+// 			Min = root.Val - root.Left.Val
+// 		}
+// 		Min = getMinimumDifference(root.Left)
+// 	}
+// 	return Min
+// }
 
 // @lc code=end
 
