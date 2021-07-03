f0a9c878287f0f10a3d9dc942dafa085951e6846
jayhyun-hwang <67496978+jayhyun-hwang@users.noreply.github.com>
2021-07-03 23:57:37 +0900

    543

--- 543.diameter-of-binary-tree.go
+++ 543.diameter-of-binary-tree.go
@@ -13,7 +13,46 @@
  *     Right *TreeNode
  * }
  */
+type DiamHeight struct {
+	diam, height int
+}
+
 func diameterOfBinaryTree(root *TreeNode) int {
+	dh := diamHeight(root)
+	return dh.diam
+}
+func diamHeight(root *TreeNode) DiamHeight {
+	dh := DiamHeight{0, -1}
+	if root == nil {
+		return dh
+	}
+	dhl := diamHeight(root.Left)
+	dhr := diamHeight(root.Right)
+	diameterWithRoot := 0
+	if root.Left != nil {
+		diameterWithRoot += 1 + dhl.height // only required if left node is present
+	}
+	if root.Right != nil {
+		diameterWithRoot += 1 + dhr.height // only required if right node is present
+	}
+	dh.diam = maxOf3(dhl.diam, dhr.diam, diameterWithRoot)
+	dh.height = 1 + maxOf2(dhl.height, dhr.height)
+	return dh
+}
+func maxOf3(a, b, c int) int {
+	if a > b && a > c {
+		return a
+	}
+	if b > c {
+		return b
+	}
+	return c
+}
+func maxOf2(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
 
 // @lc code=end
