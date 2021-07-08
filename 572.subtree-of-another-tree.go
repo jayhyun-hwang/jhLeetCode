9584f35cc455461b438ed11c75c5ebabec288ecd
jayhyun-hwang <67496978+jayhyun-hwang@users.noreply.github.com>
2021-07-08 23:02:58 +0900

    572

--- 572.subtree-of-another-tree.go
+++ 572.subtree-of-another-tree.go
@@ -26,6 +26,30 @@ func isSubtree(root *TreeNode, subRoot *TreeNode) bool {
 		return true
 	}
 }
+// func isSubtree(s *TreeNode, t *TreeNode) bool {
+//     if isSameTree(s, t) {
+//         return true
+//     }
+//     if s == nil {
+//         return false
+//     }
+//     if isSubtree(s.Left, t) || isSubtree(s.Right, t) {
+//         return true
+//     }
+//     return false
+// }
 
+// func isSameTree(t1 *TreeNode, t2 *TreeNode) bool {
+//     if t1 == nil && t2 == nil {
+//         return true
+//     }
+//     if t1 == nil || t2 == nil {
+//         return false
+//     }
+//     if t1.Val != t2.Val {
+//         return false
+//     }
+//     return isSameTree(t1.Left, t2.Left) && isSameTree(t1.Right, t2.Right)
+// }
 // @lc code=end
 
