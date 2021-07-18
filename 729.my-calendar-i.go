38436e5a1c726238efeb3142478e6f62230821b4
jayhyun-hwang <67496978+jayhyun-hwang@users.noreply.github.com>
2021-07-18 21:52:45 +0900

    729_

--- 729.my-calendar-i.go
+++ 729.my-calendar-i.go
@@ -86,6 +86,47 @@ func (this *MyCalendar) Book(start int, end int) bool {
 	return true
 }
 
+// type MyCalendar struct {
+// 	root *treeNode
+// }
+
+// type treeNode struct {
+// 	start, end  int
+// 	left, right *treeNode
+// }
+
+// func Constructor() MyCalendar {
+// 	return MyCalendar{
+// 		root: nil,
+// 	}
+// }
+
+// func (this *MyCalendar) Book(start int, end int) bool {
+// 	if end < start || start < 0 {
+// 		return false
+// 	}
+
+//     return insert(&this.root, start, end)
+// }
+
+// func insert(root **treeNode, start, end int) bool {
+// 	if *root == nil {
+// 		*root = &treeNode{
+// 			start: start,
+// 			end:   end,
+// 			left:  nil,
+// 			right: nil,
+// 		}
+// 		return true
+// 	}
+
+// 	if (*root).start >= end {
+// 		return insert(&(*root).left, start, end)
+// 	} else if (*root).end <= start {
+// 		return insert(&(*root).right, start, end)
+// 	}
+// 	return false
+// }
 /**
  * Your MyCalendar object will be instantiated and called as such:
  * obj := Constructor();
