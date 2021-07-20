d93cb0fdb851ea7a36ce579b857273f8827b7d40
jayhyun-hwang <67496978+jayhyun-hwang@users.noreply.github.com>
2021-07-20 23:43:48 +0900

    no message

---
+++ 726.number-of-atoms.go
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=726 lang=golang
+ *
+ * [726] Number of Atoms
+ */
+
+// @lc code=start
+func countOfAtoms(formula string) string {
+	//map
+	//bracket
+	//recursive function
+}
+
+// @lc code=end
+
