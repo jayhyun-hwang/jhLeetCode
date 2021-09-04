8ffc4868cf771101cc837a8b7044b9840186516f
jayhyun-hwang <67496978+jayhyun-hwang@users.noreply.github.com>
2021-09-04 23:50:09 +0900

    b1

--- python-code/algorithm-interview/B1_bitwiseOperation.py
+++ python-code/algorithm-interview/B1_bitwiseOperation.py
@@ -1,1 +1,42 @@
 # https://programmers.co.kr/learn/courses/30/lessons/17681
+
+# replace 꼭 쓰기, python의 string은 golang처럼 인덱스 순환 검사가 편하지 않으므로
+    # replace builtin 들여다보기
+    # 1. str의 n번째 문자를 바꾸고 싶다.
+    # 2. str[:n] + 문자 삽입 + str[n+1:]
+
+# zfill
+def solution(n, arr1, arr2):
+    answer = []
+    for i in range(n):
+        val = bin(arr1[i] | arr2[i])
+        val = val[2:]
+        # val = val.zfill(n)
+        for i in range(n):
+            if n != len(val):
+                val = "0" + val
+            else:
+                break
+        val = val.replace("0"," ")
+        val = val.replace("1","#")
+        answer.append(val)
+    return answer
+
+print(solution(6, [46, 33, 33 ,22, 31, 50], [27 ,56, 19, 14, 14, 10]))
+
+def valueFill(self, filling: str, length: int) -> str:
+    # 파라미터 길이 체크
+    if len(self) < 1: return
+
+    # + / - 일 때 처리
+    if self[:1] == '-' or self[:1] == '+':
+        return self.valueFill(filling, length-1)
+    
+    for i in range(length):
+        if length > len(self):
+            self = filling + self
+        else:
+            break
+    
+    return self
+
--- python-code/test.py
+++ python-code/test.py
@@ -1,1 +1,47 @@
-print(findMedianSortedArrays(self, [1,2],[1,2]))
+print("12345".zfill(3))
+# 12345
+
+print("12345".zfill(10))
+# 0000012345
+
+print("-12345".zfill(10))
+# -000012345
+
+print("+12345".zfill(10))
+# +000012345
+
+def valueFill(strParam: str, filling: str, length: int) -> str:
+    # 파라미터 길이 체크
+    if len(strParam) >= length: return strParam
+
+    # + / - 일 때 처리
+    firstIdx = strParam[:1]
+    if firstIdx == '-' or firstIdx == '+':
+        return firstIdx + valueFill(strParam[1:], filling, length-1)
+    
+    # n 길이만큼 filling 채우기
+    for i in range(length):
+        if length > len(strParam):
+            strParam = filling + strParam
+        else:
+            break
+    
+    return strParam
+
+# print(valueFill("12345", "0", 3))
+# print(valueFill("12345", "0", 10))
+# print(valueFill("-12345", "0", 10))
+# print(valueFill("+12345", "0", 10))
+# print(valueFill("+12345", "0", 5))
+# print(valueFill("+12345", "123", 6))
+# print(valueFill("+12345", "123", 4))
+# print(valueFill("-12345", "123", 5))
+# print(valueFill("-12345", "123", 6))
+# print(valueFill("-12345", "77", 7))
+# print(valueFill("-12345", "77", 4))
+
+print(valueFill("BTotheY", "얍", 10))
+print(valueFill("BTotheY", "얍얍얍", 10))
+print(valueFill("BTotheY", "얍", 30))
+
+
