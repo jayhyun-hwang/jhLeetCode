a52b998350f9bdd0e321c472dde68d5f8e856281
jayhyun-hwang <67496978+jayhyun-hwang@users.noreply.github.com>
2021-09-07 08:20:34 +0900

    auto commit

--- python-code/algorithm-interview/B3_Cache.py
+++ python-code/algorithm-interview/B3_Cache.py
@@ -1,1 +1,16 @@
 # https://programmers.co.kr/learn/courses/30/lessons/17680
+from typing import List
+
+
+def solution(cacheSize: int, cities: List[str]) -> int:
+    answer = 0
+    cache = []
+    for city in cities:
+        city = city.lower()
+        for cac in cache:
+            if city == cac:
+                answer += 1
+                break
+        
+
+    return answer
