d7231a697101d78f8ea40fa86f5f71148f909b83
jayhyun-hwang <67496978+jayhyun-hwang@users.noreply.github.com>
2021-09-07 23:25:57 +0900

    auto commit

--- python-code/algorithm-interview/B3_Cache.py
+++ python-code/algorithm-interview/B3_Cache.py
@@ -3,14 +3,29 @@ from typing import List
 
 
 def solution(cacheSize: int, cities: List[str]) -> int:
+    if cacheSize < 1:
+        return 5 * len(cities)
     answer = 0
     cache = []
     for city in cities:
+        isin = False
         city = city.lower()
         for cac in cache:
             if city == cac:
                 answer += 1
+                cache.remove(cac)
+                cache.append(city)
+                isin = True
                 break
-        
-
+        if isin:
+            continue
+        else:
+            answer += 5
+            if len(cache) < cacheSize:
+                cache.append(city)
+            else:
+                del cache[0]
+                cache.append(city)
     return answer
+
+print(solution(5, ["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "SanFrancisco", "Seoul", "Rome", "Paris", "Jeju", "NewYork", "Rome"]))
