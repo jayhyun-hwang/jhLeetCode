fe76a81213f679ca9b040f85a95ccee00d44e8f4
jayhyun-hwang <67496978+jayhyun-hwang@users.noreply.github.com>
2021-09-08 23:56:37 +0900

    auto commit

--- python-code/algorithm-interview/B4_ShuttleBus.py
+++ python-code/algorithm-interview/B4_ShuttleBus.py
@@ -3,3 +3,14 @@
 # 마지막 버스를 구한다.
 # 마지막 버스에 탈 사람들을 구한다 O(n)
 # 마지막에 탈 사람 mm -= 1
+
+from typing import List
+
+
+def solution(n: int, t: int, m: int, timetable: List[str]):
+    answer = ''
+    timetable.sort()
+    
+    return answer
+
+print(solution(2,10,2,["09:10", "09:09", "08:00"]))
