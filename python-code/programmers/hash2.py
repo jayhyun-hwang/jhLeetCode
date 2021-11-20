8960d830559bd8145155c7d9c3cf40acf203f02d
jayhyun-hwang <67496978+jayhyun-hwang@users.noreply.github.com>
2021-11-21 04:28:34 +0900

    auto commit

--- python-code/programmers/hash2.py
+++ python-code/programmers/hash2.py
@@ -1,3 +1,26 @@
-def solution(genres, plays):
-    answer = []
+from typing import List
+
+def solution(phone_book: List[str]) -> bool:
+    answer = True
+
+    dict1 = {}
+    shortestLen = 20
+    for val in phone_book:
+        if len(val) < shortestLen:
+            shortestLen = len(val)
+        dict1[val] = True
+    #print(dict1)
+    #print(shortestLen)
+    #print("12313123123"[:3])
+
+    for val in phone_book:
+        if len(val) == shortestLen:
+            continue
+        for i in range(shortestLen, len(val)):
+            if val[:i] in dict1:
+                return False
     return answer
+
+print(solution(["119", "97674223", "1195524421"]))
+print(solution(["123","456","789"]))
+print(solution(["12","123","1235","567","88"]))
