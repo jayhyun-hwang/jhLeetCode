dbb57c2ace96872b1378317514644880a345d7cd
jayhyun-hwang <67496978+jayhyun-hwang@users.noreply.github.com>
2022-02-20 07:23:35 +0900

    auto commit

---
+++ python-code/hackerrank/2-3.py
@@ -0,0 +1,27 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingSort' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+
+def countingSort(arr):
+    # Write your code here
+    res = [0 for val in range(len(arr))]
+    for val in arr:
+        res[val] += 1
+
+    return res
+
+result = countingSort([1,1,3,2,1])
+print(result)
+
