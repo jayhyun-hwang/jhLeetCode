9a99bc377d4f9f2f22e7ece0f10909ee12f190eb
jayhyun-hwang <67496978+jayhyun-hwang@users.noreply.github.com>
2022-03-25 06:42:05 +0900

    auto commit

---
+++ python-code/programmers/레벨3/20220325_1.py
@@ -0,0 +1,47 @@
+import heapq
+import sys
+from typing import List
+
+def solution(n: int, works: List[int]) -> int:
+    sys.setrecursionlimit(10 ** 9)
+    answer = 0
+
+    # 길이가 1일 때
+    if len(works) == 1:
+        temp_res = works[0] - n
+        if temp_res < 0:
+            return 0
+        return temp_res ** 2
+    
+    heap = []
+    for val in works:
+        heapq.heappush(heap, -val)
+    print(heap)
+    def find_work_heap_result(time_n, work_heap):
+        if time_n <= 0:
+            return work_heap
+        smallest = heapq.heappop(work_heap)
+        if smallest == 0:
+            return [0]
+        smallest2 = work_heap[0]
+        diff = smallest - smallest2
+
+        if diff == 0:
+            heapq.heappush(work_heap, smallest + 1)
+            time_n -= 1
+        elif time_n >= abs(diff): # 남은 time이 더 큰 경우, diff만큼만 뺌
+            heapq.heappush(work_heap, smallest - diff)
+            time_n += diff
+        else:   # 남은 time이 더 작은 경우, 남은 시간만큼 뺌
+            heapq.heappush(work_heap, smallest + time_n)
+            time_n = 0
+        return find_work_heap_result(time_n, work_heap)
+    
+    
+    res_heap = find_work_heap_result(n, heap)
+    for ele in res_heap:
+        answer += ele ** 2
+    return answer
+
+print(solution(4, [4,3,3]))
+print(solution(4, [2]))
---
+++ python-code/programmers/레벨3/20220325_2.py
@@ -0,0 +1,28 @@
+import sys
+from typing import List
+
+
+def solution(stones: List[int], k: int) -> int:
+    sys.setrecursionlimit(10 ** 9)
+    answer = 0
+    count = 0
+
+    def pass_one(stones: List[int], count: int):
+        term = 0
+        # print(stones)
+        for idx, val in enumerate(stones):
+            if val > 0:
+                term = 0
+                stones[idx] = val - 1
+                continue
+            term += 1
+            if term >= k:
+                return count
+        count += 1
+        return pass_one(stones, count)
+
+    answer = pass_one(stones, count)
+    return answer
+
+
+print(solution([2, 4, 5, 3, 2, 1, 4, 2, 5, 1], 3))
