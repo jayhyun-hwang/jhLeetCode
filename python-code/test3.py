2d66f28be1fea61afe248e43c028541d8ff781da
jayhyun-hwang <67496978+jayhyun-hwang@users.noreply.github.com>
2022-06-12 13:30:56 +0900

    ac

---
+++ python-code/test3.py
@@ -0,0 +1,9 @@
+birth_date = input("Please enter your birthdate in the format of (mmddyyyy) ")
+
+birth_month = birth_date[0:2]
+birth_day = birth_date[2:4]
+birth_year = birth_date[4:8]
+
+print("Birth Month:", birth_month)
+print("Birth Day:", birth_day)
+print("Birth Year:", birth_year)
