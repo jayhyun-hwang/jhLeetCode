734edce0b34b03b6c859c30b5a42217705684bcc
jayhyun-hwang <67496978+jayhyun-hwang@users.noreply.github.com>
2022-11-12 15:11:37 +0900

    auto commit

--- python-code/test3.py
+++ python-code/test3.py
@@ -1,11 +1,11 @@
-def solution(S, C):
-    tmp_list = []
-    cur_str = S
-    tmp_list.append(cur_str)
-    for i, ele in enumerate(C):
-        cur_num = 0
-        for ele in tmp_list:
+# def solution(S, C):
+#     tmp_list = []
+#     cur_str = S
+#     tmp_list.append(cur_str)
+#     for i, ele in enumerate(C):
+#         cur_num = 0
+#         for ele in tmp_list:
             
 
 
-print(solution("aabcddbc", [3, 5, 1, 4, 7]))  # 3
+# print(solution("aabcddbc", [3, 5, 1, 4, 7]))  # 3
