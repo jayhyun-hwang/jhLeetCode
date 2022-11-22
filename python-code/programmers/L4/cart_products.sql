f08f34e428bd65627082ccf6b6ea28ff383ac54b
jayhyun-hwang <67496978+jayhyun-hwang@users.noreply.github.com>
2022-11-23 01:59:47 +0900

    auto commit

--- python-code/programmers/L4/cart_products.sql
+++ python-code/programmers/L4/cart_products.sql
@@ -1,1 +1,15 @@
 -- https://school.programmers.co.kr/learn/courses/30/lessons/62284
+
+SELECT y.cart_id
+from (
+        select cart_id
+        from cart_products
+        where name = 'Yogurt'
+    ) y
+    join (
+        select cart_id
+        from cart_products
+        where
+            name = 'Milk'
+    ) m on y.cart_id = m.cart_id
+order by y.cart_id;
