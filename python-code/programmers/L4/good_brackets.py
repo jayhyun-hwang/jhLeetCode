816a092264f6117420076f6555f26df237479337
jayhyun-hwang <67496978+jayhyun-hwang@users.noreply.github.com>
2022-11-24 00:36:44 +0900

    auto commit

--- python-code/programmers/L4/good_brackets.py
+++ python-code/programmers/L4/good_brackets.py
@@ -1,5 +1,28 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/12929
 
+import sys
+
+sys.setrecursionlimit(10**6)
+count = 0
+def find_good(text, open_count, close_count):
+    # print('open')
+    # print(open_count)
+    # print('close')
+    # print(close_count)
+    if open_count == 0 and close_count == 0:
+        # answer_set.add(text)
+        count += 1
+        return
+    if open_count > close_count:
+        return
+    if open_count < 0:
+        find_good(text + '(', open_count - 1, close_count)
+    if close_count < 0:
+        find_good(text + ')', open_count, close_count - 1)
+
 def solution(n):
-    answer = 0
-    return answer
+    find_good('', n, n)
+    return count
+  
+print(solution(2))
+print(solution(3))
