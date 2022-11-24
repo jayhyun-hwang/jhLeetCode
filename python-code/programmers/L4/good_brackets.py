1ff961a7809437db5814017bc9c7a66f82d304f8
jayhyun-hwang <67496978+jayhyun-hwang@users.noreply.github.com>
2022-11-25 04:03:14 +0900

    auto commit

--- python-code/programmers/L4/good_brackets.py
+++ python-code/programmers/L4/good_brackets.py
@@ -3,26 +3,31 @@
 import sys
 
 sys.setrecursionlimit(10**6)
-count = 0
-def find_good(text, open_count, close_count):
-    # print('open')
-    # print(open_count)
-    # print('close')
-    # print(close_count)
+
+def find_good(text, open_count, close_count, answer_set):
     if open_count == 0 and close_count == 0:
-        # answer_set.add(text)
-        count += 1
+        answer_set.add(text)
         return
     if open_count > close_count:
         return
-    if open_count < 0:
-        find_good(text + '(', open_count - 1, close_count)
-    if close_count < 0:
-        find_good(text + ')', open_count, close_count - 1)
+    if open_count > 0:
+        find_good(text + '(', open_count - 1, close_count, answer_set)
+    if close_count > 0:
+        find_good(text + ')', open_count, close_count - 1, answer_set)
+    return
+
 
 def solution(n):
-    find_good('', n, n)
-    return count
-  
+    answer = 0
+    a_set = set()
+    find_good('', n, n, a_set)
+    # print(a_set)
+    answer = len(a_set)
+    return answer
+
+
 print(solution(2))
 print(solution(3))
+print(solution(10))
+print(solution(13))
+print(solution(15))
---
+++ python-code/programmers/L4/hotel.py
@@ -0,0 +1,5 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/64063
+
+def solution(k, room_number):
+    answer = []
+    return answer
