056220202155222b39d64135c9bd810a01271bbe
jayhyun-hwang <67496978+jayhyun-hwang@users.noreply.github.com>
2022-12-06 06:00:09 +0900

    auto commit

---
+++ python-code/programmers/L4/mukbang.py
@@ -0,0 +1,14 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/42891
+
+
+def solution(food_times, k):
+    answer = 0
+    times = k
+    one_round = 0
+    while True:
+        one_round = len(food_times)
+        # if times >= one_round:
+    return answer
+
+
+print(solution([3, 1, 2], 5))  # 1
