d295885513920720404d04fb1086e34c5d6e43b9
jayhyun-hwang <67496978+jayhyun-hwang@users.noreply.github.com>
2022-12-08 01:07:31 +0900

    auto commit

--- python-code/programmers/L4/mukbang.py
+++ python-code/programmers/L4/mukbang.py
@@ -2,12 +2,23 @@
 
 
 def solution(food_times, k):
+    if sum(food_times) <= k:
+        return -1
     answer = 0
-    times = k
-    one_round = 0
+    remain = k
+    is_complete = False
     while True:
-        one_round = len(food_times)
-        # if times >= one_round:
+        if is_complete == True:
+            break
+        for idx, val in enumerate(food_times):
+            if val > 0:
+                remain -= 1
+                if remain == -1:
+                    answer = idx + 1
+                    is_complete = True
+                    break
+                food_times[idx] = val - 1
+
     return answer
 
 
