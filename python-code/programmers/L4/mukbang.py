403a1bbc69001c18884592d908280278a98edead
jayhyun-hwang <67496978+jayhyun-hwang@users.noreply.github.com>
2022-12-10 23:48:22 +0900

    auto commit

--- python-code/programmers/L4/mukbang.py
+++ python-code/programmers/L4/mukbang.py
@@ -1,23 +1,18 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/42891
+import heapq
 
 
 def solution(food_times, k):
     if sum(food_times) <= k:
         return -1
     answer = 0
-    remain = k
-    is_complete = False
-    while True:
-        if is_complete == True:
-            break
-        for idx, val in enumerate(food_times):
-            if val > 0:
-                remain -= 1
-                if remain == -1:
-                    answer = idx + 1
-                    is_complete = True
-                    break
-                food_times[idx] = val - 1
+    h = []
+    for i in range(len(food_times)):
+        heapq.heappush(h, food_times[i])
+
+    print(h)
+    print(heapq.heapreplace())
+    print(h)
 
     return answer
 
