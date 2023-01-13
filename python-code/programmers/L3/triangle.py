d22a6be517a17d74d672b63b0aca1d4d5961a7fc
jayhyun-hwang <67496978+jayhyun-hwang@users.noreply.github.com>
2023-01-13 23:55:21 +0900

    auto commit

---
+++ python-code/programmers/L3/triangle.py
@@ -0,0 +1,13 @@
+import sys
+
+sys.setrecursionlimit(10**6)
+
+def add_sum(depth: int, )
+def solution(triangle: list[list[int]]):
+    answer = 0
+    add_sum()
+
+    return answer
+
+
+print(solution([[7], [3, 8], [8, 1, 0], [2, 7, 4, 4], [4, 5, 2, 6, 5]]))  # 30
