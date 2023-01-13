31dd45a1e12c926c6fbd203e3eb6af849a684283
jayhyun-hwang <67496978+jayhyun-hwang@users.noreply.github.com>
2023-01-14 03:40:58 +0900

    auto commit

--- python-code/programmers/L3/triangle.py
+++ python-code/programmers/L3/triangle.py
@@ -2,11 +2,25 @@ import sys
 
 sys.setrecursionlimit(10**6)
 
-def add_sum(depth: int, )
+
+def add_sum(list, last_depth: int, depth_idx: int, row_idx: int, cur_sum: int,
+            sum_list: list[int]):
+    cur_sum += list[depth_idx][row_idx]
+    print(cur_sum)
+    if depth_idx >= last_depth:
+        sum_list.append(cur_sum)
+        return
+    add_sum(list, last_depth, depth_idx + 1, row_idx, cur_sum, sum_list)
+    return add_sum(list, last_depth, depth_idx + 1, row_idx + 1, cur_sum,
+                   sum_list)
+
+
 def solution(triangle: list[list[int]]):
     answer = 0
-    add_sum()
-
+    sum_list = []
+    add_sum(triangle, len(triangle) - 1, 0, 0, 0, sum_list)
+    answer = max(sum_list)
+    print(sum_list)
     return answer
 
 
