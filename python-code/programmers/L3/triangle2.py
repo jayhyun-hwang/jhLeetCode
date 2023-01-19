ad3d1fb77e1b3c132d7cabae3c846af45ede48b5
jayhyun-hwang <67496978+jayhyun-hwang@users.noreply.github.com>
2023-01-20 00:41:48 +0900

    jh updated

--- python-code/programmers/L3/triangle2.py
+++ python-code/programmers/L3/triangle2.py
@@ -1,18 +1,22 @@
-import sys
-
-sys.setrecursionlimit(10**6)
+# import sys
 
 
+# sys.setrecursionlimit(10**6)
 def solution(triangle):
     answer = 0
-    res_list = [triangle[0]]
-    for idx, list in enumerate(triangle):
+    for idx, row in enumerate(triangle):
         if idx == 0:
             continue
-        tmp_list = []
-        for val in list:
-            tmp_list.append()
-
+        for ri, _ in enumerate(row):
+            if ri == 0:
+                row[ri] += triangle[idx - 1][ri]
+                continue
+            if ri >= len(row) - 1:
+                row[ri] += triangle[idx - 1][ri - 1]
+                continue
+            row[ri] += triangle[idx - 1][ri - 1] if triangle[idx - 1][
+                ri - 1] > triangle[idx - 1][ri] else triangle[idx - 1][ri]
+    answer = max(triangle.pop())
     return answer
 
 
