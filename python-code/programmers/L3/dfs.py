cebda3d005daffef2f61d5c5e2abbe5ac894bf6d
jayhyun-hwang <67496978+jayhyun-hwang@users.noreply.github.com>
2023-01-31 02:00:39 +0900

    jh updated

---
+++ python-code/programmers/L3/dfs.py
@@ -0,0 +1,27 @@
+import sys
+
+sys.setrecursionlimit(10 ** 6)
+
+
+def find_network(data_list, idx, visited):
+    for i, val in enumerate(data_list[idx]):
+        if i in visited:
+            continue
+        if val == 1:
+            visited.add(i)
+            find_network(data_list, i, visited)
+
+
+def solution(n, computers):
+    answer = 0
+    complited = set()
+    network = 0
+    for idx, com in enumerate(computers):
+        if idx in complited:
+            continue
+        network += 1
+        find_network(computers, idx, complited)
+    answer = network
+    return answer
+
+print(solution(3, [[1, 1, 0], [1, 1, 0], [0, 0, 1]])) # 2
