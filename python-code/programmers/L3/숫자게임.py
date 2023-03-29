1ccd61bc1b701d5661e236b08722abc6bcca8b39
Jaehyun Hwang <102587829+jaehyun-hwangf@users.noreply.github.com>
2023-03-29 18:59:07 +0900

    jh updated: Wed Mar 29 18:59:07 KST 2023

--- python-code/programmers/L3/숫자게임.py
+++ python-code/programmers/L3/숫자게임.py
@@ -1,3 +1,5 @@
+import unittest
+
 def solution(A, B):
     answer = 0
     A.sort()
@@ -19,3 +21,18 @@ def solution(A, B):
 
 print(solution([5, 1, 3, 7], [2, 2, 6, 8]))  # 3
 print(solution([2, 2, 2, 2], [1, 1, 1, 1]))  # 0
+print(solution([2, 4, 6, 8], [1, 3, 5, 7]))  # 3
+
+class TestSolution(unittest.TestCase):
+    def test_solution_1(self):
+        A = [1, 3, 5, 7]
+        B = [2, 4, 6, 8]
+        self.assertEqual(solution(A, B), 4)
+    
+    def test_solution_2(self):
+        A = [2, 4, 6, 8]
+        B = [1, 3, 5, 7]
+        self.assertEqual(solution(A, B), 3)
+
+if __name__ == '__main__':
+    unittest.main()
