8ed4825f91f0b7b3840ecbe699fb1667ce735d06
jayhyun-hwang <67496978+jayhyun-hwang@users.noreply.github.com>
2023-04-10 01:09:41 +0900

    jh updated

--- python-code/programmers/L3/보석쇼핑.py
+++ python-code/programmers/L3/보석쇼핑.py
@@ -1,6 +1,6 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/67258
 
-def solution(gems):
+def solution2(gems):
     answer = []
     gems_set = set()
     gems_length = len(gems)
@@ -22,6 +22,25 @@ def solution(gems):
     answer = [res[1][0] + 1, res[1][1] + 1]
     return answer
 
+import collections
+
+def solution(gems):
+    num = len(set(gems))
+    ret = []
+
+    left = 0
+    counter = collections.Counter()
+    for right in range(len(gems)):
+        counter[gems[right]] += 1
+        right += 1
+        while len(counter) == num:
+            counter[gems[left]] -= 1
+            if counter[gems[left]] == 0:
+                del counter[gems[left]]
+            left += 1
+            ret.append([left, right])
+
+    return sorted(ret, key = lambda x: (x[1]-x[0], x[0]))[0]
 
 print(
     solution(
