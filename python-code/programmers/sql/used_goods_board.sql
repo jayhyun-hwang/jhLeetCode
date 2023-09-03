bfe2c08b7c2efa7d7cdf712d1b34327fd15735fe
jayhyun-hwang <67496978+jayhyun-hwang@users.noreply.github.com>
2023-09-03 23:45:43 +0900

    jh updated

---
+++ python-code/programmers/sql/used_goods_board.sql
@@ -0,0 +1,5 @@
+-- 코드를 입력하세요
+select u.USER_ID, u.NICKNAME, b.TOTAL_SALES
+from (select writer_id, sum(price) as total_sales from used_goods_board where status="DONE" group by writer_id) b
+join used_goods_user u on b.writer_id=u.user_id
+where b.total_sales >= 700000 order by b.total_sales;
