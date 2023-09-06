aae416f931275888ff4ab6be2140d6aa382185e3
jayhyun-hwang <67496978+jayhyun-hwang@users.noreply.github.com>
2023-09-07 01:16:37 +0900

    jh updated

--- python-code/programmers/L3/정수삼각형.py
+++ python-code/programmers/L3/정수삼각형.py
@@ -13,3 +13,19 @@ def solution(triangle):
 
 
 print(solution([[7], [3, 8], [8, 1, 0], [2, 7, 4, 4], [4, 5, 2, 6, 5]]))  # 30
+
+def solution2(triangle):
+    if (len(triangle) <= 1):
+        return triangle.pop()
+    answer = 0
+    for i, row in enumerate(triangle):
+        if i < 1:
+            continue
+        for j, ele in enumerate(row):
+            left = 0 if j < 1 else triangle[i - 1][j - 1]
+            right = 0 if j >= len(row) - 1 else triangle[i - 1][j]
+            triangle[i][j] += max(left, right)
+    answer = max(triangle.pop())
+    return answer
+  
+print(solution2([[7], [3, 8], [8, 1, 0], [2, 7, 4, 4], [4, 5, 2, 6, 5]]))  # 30
