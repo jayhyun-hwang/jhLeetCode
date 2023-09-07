00ba862949291b71e1f3d0a72a263c54d8fa5a02
jayhyun-hwang <67496978+jayhyun-hwang@users.noreply.github.com>
2023-09-08 01:33:16 +0900

    jh updated

--- python-code/programmers/L3/이중우선순위큐.py
+++ python-code/programmers/L3/이중우선순위큐.py
@@ -28,6 +28,38 @@ def solution(operations):
             
     return answer
 
+def solution2(operations):
+    answer = []
+    max_heap = []
+    min_heap = []
+    for operation in operations:
+        # print(operation)
+        command, n = operation.split(" ")
+        n = int(n)
+        if command == "I":
+            heapq.heappush(min_heap, n)
+            heapq.heappush(max_heap, -n)
+        elif command == "D":
+            if len(max_heap) < 1:
+                continue
+            if n == 1:
+                heapq.heappop(max_heap)
+                target = min_heap
+                source_heap = max_heap
+            else:
+                heapq.heappop(min_heap)
+                target = max_heap
+                source_heap = min_heap
+            target.clear()
+            for ele in source_heap:
+                heapq.heappush(target, -ele)
+        # print(min_heap)
+        # print(max_heap)
+    if len(max_heap) < 1:
+        return [0, 0]
+    answer.append(heapq.heappop(max_heap) * -1)
+    answer.append(heapq.heappop(min_heap))
+    return answer
 
 # [0,0]
 print(solution(["I 16", "I -5643", "D -1", "D 1", "D 1", "I 123", "D -1"]))
@@ -35,6 +67,16 @@ print(solution(["I 16", "I -5643", "D -1", "D 1", "D 1", "I 123", "D -1"]))
 # [333, -45]
 print(
     solution([
+        "I -45", "I 653", "D 1", "I -642", "I 45", "I 97", "D 1", "D -1",
+        "I 333"
+    ]))
+
+# [0,0]
+print(solution2(["I 16", "I -5643", "D -1", "D 1", "D 1", "I 123", "D -1"]))
+
+# [333, -45]
+print(
+    solution2([
         "I -45", "I 653", "D 1", "I -642", "I 45", "I 97", "D 1", "D -1",
         "I 333"
     ]))
