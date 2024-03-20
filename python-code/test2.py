2b49a392ed06122e224f70ca4bf1e61bfa0d21d3
jayhyun-hwang <67496978+jayhyun-hwang@users.noreply.github.com>
2024-03-21 02:55:38 +0900

    jh updated

--- python-code/test2.py
+++ python-code/test2.py
@@ -1,17 +1,21 @@
+import collections
 def solution(S, C):
-    cur_str = S
-    for i, ele in enumerate(C):
-        print(i, ele)
-        cur_num = 0
-        def get_list(cur_num, cur_str):
-            if cur_num == ele:
-              cur_str = cur_str[:cur_num] + "$" + cur_str[cur_num:]
-              return cur_str
-            if cur_str[cur_num] != "$":
-                cur_num += 1
-                return get_list(cur_num, cur_str)
-        res = get_list(cur_num, cur_str)
-        print(res)
+    # cur_str = S
+    # for i, ele in enumerate(C):
+    #     print(i, ele)
+    #     cur_num = 0
+    #     def get_list(cur_num, cur_str):
+    #         if cur_num == ele:
+    #           cur_str = cur_str[:cur_num] + "$" + cur_str[cur_num:]
+    #           return cur_str
+    #         if cur_str[cur_num] != "$":
+    #             cur_num += 1
+    #             return get_list(cur_num, cur_str)
+    #     res = get_list(cur_num, cur_str)
+    #     print(res)
+    d = collections.deque()
+    d.append(1)
+    print(d)
 
             
 
