22a8302c13ebf86a22865329a8eb77016d01e837
jayhyun-hwang <67496978+jayhyun-hwang@users.noreply.github.com>
2024-10-04 03:35:41 +0900

    jh updated

--- python-code/leetCode/856.score-of-parentheses.py
+++ python-code/leetCode/856.score-of-parentheses.py
@@ -7,6 +7,26 @@
 # @lc code=start
 class Solution:
     def scoreOfParentheses(self, s: str) -> int:
-        
-# @lc code=end
+        answer = 0
+        stack = []
+        for ele in s:
+            if ele == "(":
+                stack.append(ele)
+                continue
+            vertex = stack.pop()
+            if vertex == "(":
+                stack.append(1)
+                continue
+            temp_num = vertex
+            while vertex != "(":
+                vertex = stack.pop()
+                if vertex == "(":
+                    temp_num *= 2
+                    stack.append(temp_num)
+                else:
+                    temp_num += vertex
 
+        answer = sum(stack)
+        return answer
+
+# @lc code=end
