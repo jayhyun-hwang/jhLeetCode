ea59492ee93b40978ace5edda5b38d5b93179394
jayhyun-hwang <67496978+jayhyun-hwang@users.noreply.github.com>
2024-10-29 02:22:42 +0900

    jh updated

--- python-code/leetCode/875.koko-eating-bananas.py
+++ python-code/leetCode/875.koko-eating-bananas.py
@@ -4,9 +4,31 @@
 # [875] Koko Eating Bananas
 #
 
-# @lc code=start
-class Solution:
-    def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        
-# @lc code=end
+import heapq
 
+
+class Solution:
+    def minEatingSpeed(self, piles, h: int) -> int:
+        if len(piles) == 1:
+            q = piles[0] // h
+            r = piles[0] % h
+            if r > 0:
+                q += 1
+            return q
+
+        heap = []
+        for p in piles:
+            heapq.heappush(heap, -p)
+        cur_min = heap[0]
+        count = h - len(piles)
+        # print(cur_min, count)
+        while count > 0:
+            # print(heap)
+            a = -heapq.heappop(heap)
+            q = a // 2
+            r = a % 2
+            q = q + 1 if r > 0 else q
+            heapq.heappush(heap, -q)
+            cur_min = heap[0]
+            count -= 1
+        return -cur_min
