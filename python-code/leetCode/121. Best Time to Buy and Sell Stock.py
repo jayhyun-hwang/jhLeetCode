e16731e6c948700139ac46cac20d563bf50325c7
jayhyun-hwang <wogus0501@gmail.com>
2025-06-18 03:19:46 +0900

    jh updated: Wed Jun 18 03:19:46 KST 2025

--- python-code/leetCode/121. Best Time to Buy and Sell Stock.py
+++ python-code/leetCode/121. Best Time to Buy and Sell Stock.py
@@ -1,8 +1,28 @@
 # https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
+import heapq
+
+
 class Solution:
     def maxProfit(self, prices: list[int]) -> int:
-        return
+        min_heap = []
+
+        max_profit = 0
+        for idx, val in enumerate(prices):
+            if idx+1 >= len(prices):
+                continue
+            # print(prices[idx+1:])
+            heapq.heappush(min_heap, val)
+            max_heap = prices[idx+1:]
+            max_heap = [-x for x in max_heap]
+            heapq.heapify(max_heap)
+
+            # print(max_heap, min_heap)
+            diff = -max_heap[0] - min_heap[0]
+            max_profit = max_profit if diff < max_profit else diff
+
+        return max_profit
 
 
 s = Solution()
 print(s.maxProfit([7, 1, 5, 3, 6, 4]))  # 5
+print(s.maxProfit([7, 11, 1, 2, 4]))  # 4
