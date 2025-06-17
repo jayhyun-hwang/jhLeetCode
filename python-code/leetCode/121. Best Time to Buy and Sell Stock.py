024534075b50188c6a2c018d9996e08c68fe3a52
jayhyun-hwang <wogus0501@gmail.com>
2025-06-17 23:57:25 +0900

    jh updated: Tue Jun 17 23:57:25 KST 2025

--- python-code/leetCode/121. Best Time to Buy and Sell Stock.py
+++ python-code/leetCode/121. Best Time to Buy and Sell Stock.py
@@ -1,17 +1,8 @@
 # https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
 class Solution:
-    def hammingWeight(self, n: int) -> int:
-        q = n
-        bits = 0
-        while q >= 1:
-            r = q % 2
-            if r == 1:
-                bits += 1
-            q = q // 2
-        return bits
+    def maxProfit(self, prices: list[int]) -> int:
+        return
 
 
 s = Solution()
-print(s.hammingWeight(11))  # 3
-print(s.hammingWeight(128))  # 1
-print(s.hammingWeight(2147483645))  # 30
+print(s.maxProfit([7, 1, 5, 3, 6, 4]))  # 5
